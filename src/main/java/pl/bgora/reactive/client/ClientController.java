c8d75149e7e04fe75e00b8facede15539535260a
bgora <bartlomiej.gora@infobip.com>
2020-12-09 17:13:59 +0100

    reactive client - back pressure

--- src/main/java/pl/bgora/reactive/client/ClientController.java
+++ src/main/java/pl/bgora/reactive/client/ClientController.java
@@ -1,8 +1,10 @@
 package pl.bgora.reactive.client;
 
+import org.reactivestreams.Subscription;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.BaseSubscriber;
 
 @RestController
 public class ClientController {
@@ -10,16 +12,30 @@ public class ClientController {
     private final WebClient webClient;
 
     public ClientController() {
-        webClient = WebClient.create("localhost:8080/data");
+        webClient = WebClient.create("localhost:8090/data");
     }
 
     @GetMapping("getData")
     public void getData() {
         webClient.get()
-                .exchangeToFlux(clientResponse -> clientResponse.bodyToFlux(String.class))
-                .limitRate(1)
-                .doOnNext(System.out::println)
-                .log()
-                .subscribe();
+                 .exchangeToFlux(clientResponse -> clientResponse.bodyToFlux(Integer.class))
+                 .log()
+        .subscribeWith(new BaseSubscriber<Integer>() {
+
+            private int count = 0;
+            @Override
+            protected void hookOnSubscribe(Subscription subscription) {
+                request(5);
+            }
+
+            @Override
+            protected void hookOnNext(Integer value) {
+                System.out.println("Count:" + ++count);
+                System.out.println("value" + value);
+                if(count >=5) {
+                    cancel();
+                }
+            }
+        });
     }
 }
